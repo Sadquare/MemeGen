ff5d5c03c586daa4776bd3fa0f04baf15230dfe9
unknown <idrissasadade0205@gmail.com>
2025-04-21 04:46:21 +0100

    logo update

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Header = () => {
   return (
     <header className="flex justify-center items-center p-5 bg-gradient-to-r from-blue-500 to-blue-700 text-white shadow-lg">
-      <img src="../public/meme.png" alt="logo" className="w-20" />
       <h1 className="ml-5 text-5xl font-bold">Meme Generator</h1>
     </header>
   )
